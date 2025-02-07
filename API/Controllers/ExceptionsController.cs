c4a20b83d2060b858d0264604ca1bcc8a26e999a
kit-foxboy <admin@vulpineinteractive.com>
2025-02-06 18:32:45 -0700

    Added dummy api error for front end testing

--- API/Controllers/ExceptionsController.cs
+++ API/Controllers/ExceptionsController.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -12,4 +13,11 @@ public class ExceptionsController : BaseApiController
   {
     throw new Exception("Test exception");
   } 
+
+  [AllowAnonymous]
+  [HttpGet("server-error")]
+  public ActionResult TestServerException()
+  {
+    throw new Exception("Server go explode");
+  }
 }
